eadc56f2677140f851cd5f026547c523ecca8976
Pascal7601 <levispascal63@gmail.com>
2026-01-01 11:05:19 +0300

    feat: moved from localhost to prod' '

--- users/tasks.py
+++ users/tasks.py
@@ -8,7 +8,7 @@ def send_verification_email(user_id, token):
     """"Sends a verification email to the user with the provided token."""
 
     user = User.objects.get(id=user_id)
-    verification_link = f"http://localhost:5173/auth/verify-email/?token={token}"
+    verification_link = f"https://werah.me/auth/verify-email/?token={token}"
     subject = "Verify your email address"
     message = f"Hi {user.first_name},\n\nPlease verify your email address by clicking the link below:\n{verification_link}\n\nThank you!"
     recipient_list = [user.email]
